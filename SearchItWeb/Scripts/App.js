5170da252bb8bde2eecbd79a622a7514fdd2cf0f
maxali <maxali.abdi@gmail.com>
2016-01-26 02:05:49 +0100

    added MVC

---
+++ SearchItWeb/Scripts/App.js
@@ -0,0 +1,95 @@
+﻿/* Common app functionality */
+
+var app = (function () {
+    "use strict";
+
+    var app = {};
+
+    // Common initialization function (to be called from each page)
+    app.initialize = function () {
+        $.support.cors = true;
+        $.ajaxSetup({
+            crossDomain: true,
+            dataType: 'json'
+        });
+
+        if ($.fn.Pivot) { $('.ms-Pivot').Pivot(); }
+        if ($.fn.SearchBox) { $('.ms-SearchBox-field').SearchBox(); }
+        
+
+
+        buildNotification();
+    };
+
+    app.getSelectedData = _getDataFromSelection;
+    app.setBinding = _setBindings;
+    app.addBinding = _addBinding;
+
+    function _addBinding(bindingId) {
+        Office.context.document.bindings.addFromSelectionAsync(Office.BindingType.Text, { id: bindingId }, function (result) {
+            if (result.status === Office.AsyncResultStatus.Succeeded) {
+                app.showNotification("Binding!", "Binding added successfully");
+            } else {
+                app.showNotification("Error", result.error.message);
+            }
+        });
+    }
+
+    function _setBindings(id, val, _coercionType) {
+        var coercionType = _coercionType || Office.CoercionType.Text;
+        Office.select("bindings#" + id, function () {
+            console.error("Binding " + id + " not found!");
+        }).setDataAsync(
+          val,
+          { coercionType: coercionType },
+          function (asyncResult) {
+              if (asyncResult.status == "failed") {
+                  console.error("Action failed with error: " + asyncResult.error.message);
+              } else {
+                  console.log("Updated with " + val);
+              }
+          }
+        );
+
+    }
+
+    function _getDataFromSelection() {
+        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
+            function (result) {
+                if (result.status === Office.AsyncResultStatus.Succeeded) {
+                    return result.value || "";
+                } else {
+                    return null; //result.error.message;
+                }
+            }
+        );
+    }
+
+    function buildNotification() {
+        $('body').append(
+            '<div id="notification-message">' +
+                '<div class="padding">' +
+                    '<div id="notification-message-close"></div>' +
+                    '<div id="notification-message-header"></div>' +
+                    '<div id="notification-message-body"></div>' +
+                '</div>' +
+            '</div>');
+
+        
+        $('#notification-message-close').click(app.hideNotification);
+
+
+        // After initialization, expose a common notification function
+        app.showNotification = function (header, text) {
+            $('#notification-message-header').text(header);
+            $('#notification-message-body').text(text);
+            $('#notification-message').slideDown('fast');
+        };
+
+        app.hideNotification = function () {
+            $('#notification-message').hide();
+        };
+    }
+
+    return app;
+})();
---
+++ SearchItWeb/Scripts/Home.js
@@ -0,0 +1,63 @@
+﻿/// <reference path="../App.js" />
+/// <reference path="../../Content/js/SPLogin/SPTemp.js" />
+
+(function () {
+    "use strict";
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+            app.data = [
+                { "ID": 1, "Name": "Mr Hakin Andrin", "Tel": "95330558" },
+                { "ID": 2, "Name": "Mr Sharepoint Office", "Tel": "98554855" }
+            ];
+
+            /**
+            Home.html event handlers
+            **/
+            var elSearch = $('.ms-SearchBox-field');
+            elSearch.keypress(function (e) {
+                if (e.which == 13) {
+                    app.search.crossDomainAjaxCall($(this).val());
+                }
+            });
+
+            $('#get-data-from-selection').click(function () {
+                app.addBinding(elSearch.val());
+            });
+
+            $('#login').click(function () {
+                SPTemp.login("mohamed@crayonsky.onmicrosoft.com",
+                    "JaalleDheere10",
+                    "http://crayonsky.sharepoint.com",
+                    function () {
+                        console.log("Login success");
+                    },
+                    function () {
+                        console.log("login fail");
+                    })
+            });
+
+            $('.ms-Pivot-link').click(function () {
+                //build tab-ing view
+                var targetTab = $('#tab-' + $(this).attr('data-tab-target'));
+                $('.pivot-tabs .tab').removeClass('selected');
+                targetTab.addClass('selected');
+
+                // bind click to action
+                if (!targetTab.hasClass('no-search') && elSearch.val().length > 0)
+                    app.search.crossDomainAjaxCall(elSearch.val());
+            });
+
+            $('#filldata').click(function () {
+                $.each(app.data, function (i, item) {
+                    for (var prop in item) {
+                        app.setBinding(prop, item[prop]);
+                    }
+                });
+            });
+
+
+        });
+    };
+})();
